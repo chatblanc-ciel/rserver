2ed9580cf83f13b8a4d7ea1ef1e750821c1f2420
chatblanc-ciel <matsumoto9845@spice.ocn.ne.jp>
2021-11-15 19:35:00 +0900

    create Http request analyzer

--- src/web_dealer/mod.rs
+++ src/web_dealer/mod.rs
@@ -10,20 +10,22 @@ use self::dealer_error::DealerError;
 
 /// # WebDealer
 ///
-/// working to deliver HTTP Response by analizing listened HTTP Request.
+/// working to deliver Http Response by analizing listened Http Request.
 ///
-/// # Input
-///
-/// - listening address
-///
-/// # Error
-///
-/// Can not connect to listening address.
 pub struct WebDealer<T> {
     listener: Arc<TcpListener>,
     worker: JoinHandle<T>,
 }
 impl WebDealer<()> {
+    /// Generate ``WebDealer``.
+    ///
+    /// # Input
+    ///
+    /// - listening address
+    ///
+    /// # Error
+    ///
+    /// Can not connect to listening address.
     pub fn new<A>(addr: A) -> Result<Self, DealerError>
     where
         A: ToSocketAddrs,
@@ -40,11 +42,43 @@ impl WebDealer<()> {
         }
 
         // Second Step : Generate worker thread
-        let thread = thread::spawn(move || {});
+        let listener_cp = Arc::clone(&listener);
+        let thread = thread::spawn(move || {
+            for stream in listener_cp.incoming() {
+                let stream = stream.unwrap();
+
+                Self::handle_connection(stream);
+            }
+        });
 
         Ok(Self {
             listener,
             worker: thread,
         })
+    }
+
+    /// analyze Http Request
+    ///
+    fn handle_connection(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+
+        let get = b"GET / HTTP/1.1\r\n";
+
+        let (status_line, filename) = if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK\r\n\r\n", "./static/top.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "./static/404.html")
+        };
+
+        let mut file = File::open(filename).unwrap();
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
